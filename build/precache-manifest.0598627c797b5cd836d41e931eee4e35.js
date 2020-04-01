00d00fa128d3463fc733ad402c38a8d751d6971d
shailesh <shaileshmourya1995@gmail.com>
2020-04-01 21:11:46 +0530

    build

---
+++ build/precache-manifest.0598627c797b5cd836d41e931eee4e35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27c87657027c309553e2d2680c37dd76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82bedc8e0240ebc444fe",
+    "url": "/static/css/main.e2bfd105.chunk.css"
+  },
+  {
+    "revision": "dd84f3db4ac407200b07",
+    "url": "/static/js/2.50364907.chunk.js"
+  },
+  {
+    "revision": "82bedc8e0240ebc444fe",
+    "url": "/static/js/main.6cf623db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
